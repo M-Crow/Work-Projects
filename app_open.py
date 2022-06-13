106d84b6daf4e76c937b22c90e46dde44586378b
M-Crow <Mcrowbb24@gmail.com>
2022-06-13 13:34:30 -0400

    commit

--- app_open.py
+++ app_open.py
@@ -1,3 +1,10 @@
 # script to open windows apps on login
 import os
+import webbrowser
 
+# opens outlook
+os.startfile("Outlook")
+
+# opens toggle and n-able in firefox 
+webbrowser.open("https://track.toggl.com/timer")
+webbrowser.open("https://www.n-able.com/product-login")
